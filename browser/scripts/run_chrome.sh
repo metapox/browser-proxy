5de18e3c0235caf0504947bb5508842675801108
metapox <84276752+metapox@users.noreply.github.com>
2023-06-07 08:24:50 +0900

    proxy setting

--- browser/scripts/run_chrome.sh
+++ browser/scripts/run_chrome.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # run_chrome.sh
-google-chrome-stable --headless --disable-gpu --screenshot --virtual-time-budget=5000 --no-sandbox --window-size=1280x1024 $1
+proxy_server="mitmproxy" # あなたのプロキシサーバのアドレス
+proxy_port="8080"     # あなたのプロキシサーバのポート
+
+google-chrome-stable --headless \
+                     --disable-gpu \
+                     --virtual-time-budget=5000 \
+                     --no-sandbox \
+                     --proxy-server="$proxy_server:$proxy_port" \
+                     $1
