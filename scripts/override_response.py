a472a3bd81f99cc00540dbe1586db8c67d47af6e
metapox <84276752+metapox@users.noreply.github.com>
2023-07-09 13:53:07 +0900

    file override

--- scripts/override_response.py
+++ scripts/override_response.py
@@ -1,6 +1,22 @@
-# override_response.py
 from mitmproxy import http
+from urllib.parse import urlparse
+import os
+import logging
+
+log_file = '/var/log/mitmproxy/app.log'
+base_dir = '/home/mitmproxy/override-files/'
+
+logging.basicConfig(filename=log_file, level=logging.INFO)
 
 def response(flow: http.HTTPFlow) -> None:
-    if flow.response.headers.get("content-type", "").startswith("text/html"):
-        flow.response.content = flow.response.content.replace(b"</body>", b"<p>override</p></body>")
+    url = urlparse(flow.request.pretty_url)
+    path = os.path.join(base_dir, url.netloc, url.path.strip('/'))
+
+    if os.path.isfile(path):
+        logging.info('Override response for %s', flow.request.pretty_url)
+        with open(path, 'r') as f:
+            flow.response = http.HTTPResponse.make(
+                200,
+                f.read(),
+                {"Content-Type": "text/html"}
+            )
