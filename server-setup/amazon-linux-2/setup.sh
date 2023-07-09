8b93964badb2a44742c3fb075b700bc369059bcc
metapox <84276752+metapox@users.noreply.github.com>
2023-07-09 18:49:25 +0900

    fix path

--- server-setup/amazon-linux-2/setup.sh
+++ server-setup/amazon-linux-2/setup.sh
@@ -2,21 +2,22 @@
 
 # Install mitmproxy
 yum install -y python3 python3-pip procps git
-pip install mitmproxy
+pip3 install mitmproxy
 
 # making log dir
 mkdir -p /var/log/mitmproxy/
-chown -R ec2-user:ec2-user /var/log/mitmproxy/
 touch /var/log/mitmproxy/mitmproxy.log
-chown -R ec2-user:ec2-user /var/log/mitmproxy/mitmproxy.log
 touch /var/log/mitmproxy/app.log
+chown -R ec2-user:ec2-user /var/log/mitmproxy/
+chown -R ec2-user:ec2-user /var/log/mitmproxy/mitmproxy.log
 chown -R ec2-user:ec2-user /var/log/mitmproxy/app.log
 
-git clone https://github.com/metapox/browser-proxy.git
+git clone https://github.com/metapox/browser-proxy.git /home/mitmproxy
+cd /home/mitmproxy/
 conf_dir=$(pwd)/browser-proxy/server-setup/amazon-linux-2/
 
 # logrotate
-/etc/rc.d/init.d/crond start
+systemctl crond start
 cp $conf_dir/logrotate.conf /etc/logrotate.d/mitmproxy
 
 # mitmproxy.service
