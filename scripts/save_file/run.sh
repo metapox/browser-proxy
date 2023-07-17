518d25c8da78bda4feaaaa074042abe577a20c96
metapox <84276752+metapox@users.noreply.github.com>
2023-07-17 12:00:05 +0900

    save file setup

---
+++ scripts/save_file/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+start() {
+    echo "Starting process..."
+    /usr/local/bin/mitmdump -p 9090 -s /home/mitmproxy/scripts/save_file/save_file.py --ssl-insecure --mode regular > /var/log/mitmproxy/save_file.log &
+}
+
+stop() {
+    echo "Stopping process"
+    pkill -f "/usr/local/bin/mitmdump -p 9090 -s /home/mitmproxy/scripts/override_response.py --ssl-insecure --mode regular"
+    echo "Done"
+}
+
+restart() {
+    echo "Restarting process..."
+    stop
+    start
+}
+
+cas "$1" in
+    start)   start ;;
+    stop)    stop ;;
+    restart) restart ;;
+    *) echo "usage: $0 start|stop|restart" >&2
+       exit 1
+       ;;
+esac
--- scripts/save_file/save_file.py
+++ scripts/save_file/save_file.py
@@ -2,16 +2,24 @@ from mitmproxy import http
 from urllib.parse import urlparse
 import os
 import logging
+import yaml
 
 base_dir = '/home/mitmproxy/override-files/'
-targets = [
-]
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+config_path = os.path.join(script_dir, 'config.yaml')
+
+with open(config_path, 'r') as stream:
+  try:
+    data = yaml.safe_load(stream)
+  except yaml.YAMLError as exc:
+    print(exc)
 
 def response(flow: http.HTTPFlow) -> None:
   url = urlparse(flow.request.pretty_url)
   path = os.path.join(url.netloc, url.path.lstrip('/'))
 
-  if (path in targets):
+  if (path in data['targets']):
     path = os.path.join(base_dir, path)
 
     if (path[len(path) - 1] == '/'):
